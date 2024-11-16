05bb3f0ead0591c969af980e0deff503eeb10419
KauaiMoho <113269300+KauaiMoho@users.noreply.github.com>
2024-11-15 22:44:20 -0500

    data

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('Hello')
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data_chsl = pd.read_csv('heart_disease_chsl.csv')
+data_framingham = pd.read_csv('heart_disease_framingham.csv')
+data_iran = pd.read_csv('heart_disease_iran.csv')
+
+print(data_iran)
