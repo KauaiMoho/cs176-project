4661f29f62b4d1956264f01c5d2ef7ef8ed1bf6a
KauaiMoho <113269300+KauaiMoho@users.noreply.github.com>
2024-11-17 18:09:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data_iran = pd.read_csv('heart_disease_iran.csv')
-data_chsl = pd.read_csv('heart_disease_chsl.csv')
+data_chsl = pd.read_csv('heart_disease_chsl.csv') #Manit
 data_framingham = pd.read_csv('heart_disease_framingham.csv')
-
-print(data_iran.columns)
-print(data_chsl.columns)
-print(data_framingham.columns)
+ 
+#drop data with missing values in cloestrol and blood sugar cols
+data_framingham_cleaned = data_framingham.dropna(subset=['GLUCOSE'])
+#isolate patients with heart disease at exam
+data_framingham_cleaned = data_framingham_cleaned.loc[data_framingham_cleaned['PREVCHD'] == 1] 
 
 # age, sex, heart rate, resting/diastolic blood pressure, blood sugar
 # data_merged_lossless = data_iran.concat()
 
-#Heart Rate Outliers
 
 #heartRateOutlierUpperBound = data_chsl["Heart Rate"].quantile(0.75) + 1.5 * (data_chsl["Heart Rate"].quantile(0.75) - data_chsl["Heart Rate"].quantile(0.25))
 
@@ -20,5 +20,3 @@ data_iran_cleaned = data_iran.loc[(data_iran["Blood sugar"] < 541) & (data_iran[
 
 print(data_iran_cleaned)
 
-#pd.plotting.scatter_matrix(data_iran, alpha=0.5, figsize=(10, 10), diagonal='hist')
-#plt.show()
