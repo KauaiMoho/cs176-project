3cb7878187408e4566a0808181cd083895d0a1ff
Rohit Sattuluri <rsattulu@purdue.edu>
2024-11-17 18:08:58 -0500

    started cleaning iran data

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ print(data_framingham.columns)
 
 #Heart Rate Outliers
 
+#heartRateOutlierUpperBound = data_chsl["Heart Rate"].quantile(0.75) + 1.5 * (data_chsl["Heart Rate"].quantile(0.75) - data_chsl["Heart Rate"].quantile(0.25))
 
-pd.plotting.scatter_matrix(data_iran, alpha=0.5, figsize=(10, 10), diagonal='hist')
-plt.show()
+data_iran_cleaned = data_iran.loc[(data_iran["Blood sugar"] < 541) & (data_iran["Result"] == "positive")]
+
+print(data_iran_cleaned)
+
+#pd.plotting.scatter_matrix(data_iran, alpha=0.5, figsize=(10, 10), diagonal='hist')
+#plt.show()
